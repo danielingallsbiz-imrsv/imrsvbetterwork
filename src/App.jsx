72ed4278545a620905e2e1f6148fd2980068c163
Daniel Ingalls <danielingalls@Daniels-MacBook-Air.local>
2026-02-23 11:22:00 -0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+export default function App() {
+  return (
+    <div
+      style={{
+        background: "#f8f8f6",
+        minHeight: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <img
+        src="/logo.png"
+        alt="IMRSV Logo"
+        style={{ height: 140, width: "auto" }}
+      />
+    </div>
+  );
+}
